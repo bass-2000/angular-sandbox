57df761fd2ca34bcfeeb67608ce010297fec3560
alexanderboyko <bass-2000@yandex.ru>
2020-04-07 17:42:59 +0300

    component creation by hands and by cli

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3{ color: coral } `]
 })
 export class AppComponent {
   title = 'my-first-app';
