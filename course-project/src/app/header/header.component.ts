494463cc83425cf22b95dcee812bece0becd80e7
alexanderboyko <bass-2000@yandex.ru>
2020-04-15 19:41:51 +0300

    course project navigation via ngif added

--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styles: []
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() valueSelected = new EventEmitter<string>();
+  onSelect(value: string) {
+    this.valueSelected.emit(value);
+  }
+}
