e3c303c51c618c2e9f2319c312bc2691a9f6279a
alexanderboyko <bass-2000@yandex.ru>
2020-04-26 10:24:49 +0300

    course project shopping list service done

--- course-project/src/app/shopping-list/shopping-list.service.ts
+++ course-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+private ingredients: Ingredient[] = [
+  new Ingredient('Apples', 99),
+  new Ingredient('Tomatoes', 99)
+];
+
+getIngredients(){
+  return this.ingredients.slice();
+}
+
+addIngredient(ingredient: Ingredient){
+  this.ingredients.push(ingredient);
+  this.ingredientsChanged.emit(this.ingredients.slice());
+}
 }
