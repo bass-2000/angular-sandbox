ced48d99c3a8122639340f6758b8a09e8f8ef535
alexanderboyko <bass-2000@yandex.ru>
2020-04-28 12:23:07 +0300

    routing-guide-app navigation routes and styling

--- routing-guide-app/src/app/app-routing.module.ts
+++ routing-guide-app/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UsersComponent} from './users/users.component';
+import {HomeComponent} from './home/home.component';
+import {ServersComponent} from './servers/servers.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent },
+  { path: '', component: HomeComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
