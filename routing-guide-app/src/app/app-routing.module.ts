26909423210920008e31c853ce0f1fa20c06310e
alexanderboyko <bass-2000@yandex.ru>
2020-04-30 11:09:13 +0300

    routing-guide-app passing params to routes, fetching reactively

--- routing-guide-app/src/app/app-routing.module.ts
+++ routing-guide-app/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {UsersComponent} from './users/users.component';
 import {HomeComponent} from './home/home.component';
 import {ServersComponent} from './servers/servers.component';
+import {UserComponent} from './users/user/user.component';
 
 
 const routes: Routes = [
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent },
   { path: '', component: HomeComponent }
   ];
