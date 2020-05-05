649d151237f1b590902b77b2c87b63677370e8bd
alexanderboyko <bass-2000@yandex.ru>
2020-05-05 08:10:18 +0300

    routing-guide-app wildcards routing

--- routing-guide-app/src/app/app-routing.module.ts
+++ routing-guide-app/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {ServersComponent} from './servers/servers.component';
 import {UserComponent} from './users/user/user.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
@@ -16,7 +17,9 @@ const routes: Routes = [
       { path: ':id/edit', component: EditServerComponent },
       { path: ':id', component: ServerComponent },
     ] },
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
   ];
 
 @NgModule({
