010dfeb643345e860a61cb397b9d7792a9c0efe7
alexanderboyko <bass-2000@yandex.ru>
2020-05-06 09:21:14 +0300

    routing-guide-app Protecting Route with canActivate

--- routing-guide-app/src/app/app-routing.module.ts
+++ routing-guide-app/src/app/app-routing.module.ts
@@ -7,13 +7,14 @@ import {UserComponent} from './users/user/user.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AuthGuard} from './auth-guard.service';
 
 
 const routes: Routes = [
   { path: 'users', component: UsersComponent, children: [
       { path: ':id/:name', component: UserComponent },
     ] },
-  { path: 'servers', component: ServersComponent, children: [
+  { path: 'servers', canActivate:[AuthGuard], component: ServersComponent, children: [
       { path: ':id/edit', component: EditServerComponent },
       { path: ':id', component: ServerComponent },
     ] },
@@ -22,8 +23,10 @@ const routes: Routes = [
   { path: '**', redirectTo: '/not-found' }
   ];
 
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+// @ts-ignore
 export class AppRoutingModule { }
