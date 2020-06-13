ebdb139e544868a87977e0e4579aefd0352b9c1b
alexanderboyko <bass-2000@yandex.ru>
2020-06-13 11:47:30 +0300

    http-guide How Does Angular Interact With Backends The Anatomy of a Http Request Backend (Firebase) Setup Sending a POST Request GETting Data

---
+++ http-guide/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+   myUrl = 'https://angularlearing.firebaseio.com/posts.json';
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http
+      .post(
+        this.myUrl,
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get(this.myUrl).subscribe(posts => {
+      console.log(posts);
+    });
+  }
+}
