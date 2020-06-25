a732ada412677f7671a61594afa546c738b5d630
alexanderboyko <bass-2000@yandex.ru>
2020-06-25 20:55:12 +0300

    course-project Setting Up the DataStorage Service Storing Recipes Fetching Recipes

---
+++ course-project/src/app/shared/data-storage.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+   private myURL = 'https://ng-course-recipe-book-bass2000.firebaseio.com/recipes.json';
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        this.myURL,
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        this.myURL
+      )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        })
+      )
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      });
+  }
+}
