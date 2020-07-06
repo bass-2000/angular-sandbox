40b424dec23cb68845779411a70ca7d67de97609
alexanderboyko <bass-2000@yandex.ru>
2020-07-06 16:16:30 +0300

    course-project Adding the Token to Outgoing Requests

--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {exhaustMap, map, take, tap} from 'rxjs/operators';
 
-import { Recipe } from '../recipes/recipe.model';
-import { RecipeService } from '../recipes/recipe.service';
+import {Recipe} from '../recipes/recipe.model';
+import {RecipeService} from '../recipes/recipe.service';
+import {AuthService} from '../auth/auth.service';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class DataStorageService {
-   private myURL = 'https://ng-course-recipe-book-bass2000.firebaseio.com/recipes.json';
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  private myURL = 'https://ng-course-recipe-book-bass2000.firebaseio.com/recipes.json';
+
+  constructor(private http: HttpClient,
+              private recipeService: RecipeService,
+              private authService: AuthService) {
+  }
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -23,22 +28,22 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http
-      .get<Recipe[]>(
-        this.myURL
-      )
-      .pipe(
-        map(recipes => {
-          return recipes.map(recipe => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
-            };
-          });
-        }),
-        tap(recipes => {
-          this.recipeService.setRecipes(recipes);
-        })
-      );
+    return this.authService.user.pipe(take(1), exhaustMap(user => {
+        return this.http
+          .get<Recipe[]>(
+            this.myURL,
+            {params: new HttpParams().set('auth', user.token)}
+          );
+      }), map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      }));
   }
 }
