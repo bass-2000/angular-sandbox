55f3bed6036a5e8f883f86aa7e2e54f78ad0fe9e
alexanderboyko <bass-2000@yandex.ru>
2020-08-04 18:08:24 +0300

    course-project Managing More State via NgRx Removing Redundant Component State Management

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -17,25 +17,36 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   @ViewChild('f', {static: false}) slForm: NgForm;
   subscription: Subscription;
   editMode = false;
-  editedItemIndex: number;
   editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService, private store: Store<fromShoppingList.AppState>) {
   }
 
   ngOnInit() {
-    this.subscription = this.slService.startedEditing
-      .subscribe(
-        (index: number) => {
-          this.editedItemIndex = index;
-          this.editMode = true;
-          this.editedItem = this.slService.getIngredient(index);
-          this.slForm.setValue({
-            name: this.editedItem.name,
-            amount: this.editedItem.amount
-          });
-        }
-      );
+    this.subscription = this.store.select('shoppingList').subscribe(stateData => {
+      if (stateData.editedIngredientIndex > -1) {
+        this.editMode = true;
+        this.editedItem = stateData.editedIngredient;
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
+      } else {
+        this.editMode = false;
+      }
+    });
+    // this.subscription = this.slService.startedEditing
+    //   .subscribe(
+    //     (index: number) => {
+    //       this.editedItemIndex = index;
+    //       this.editMode = true;
+    //       this.editedItem = this.slService.getIngredient(index);
+    //       this.slForm.setValue({
+    //         name: this.editedItem.name,
+    //         amount: this.editedItem.amount
+    //       });
+    //     }
+    //   );
   }
 
   onSubmit(form: NgForm) {
@@ -44,12 +55,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     if (this.editMode) {
       // this.slService.updateIngredient(this.editedItemIndex, newIngredient);
       this.store.dispatch(
-        new ShoppingListActions.UpdateIngredient(
-          {
-            index: this.editedItemIndex,
-            ingredient: newIngredient
-          }
-        )
+        new ShoppingListActions.UpdateIngredient(newIngredient)
       );
     } else {
       // this.slService.addIngredient(newIngredient);
@@ -62,17 +68,19 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onClear() {
     this.slForm.reset();
     this.editMode = false;
+    this.store.dispatch(new ShoppingListActions.StopEdit());
   }
 
   onDelete() {
     // this.slService.deleteIngredient(this.editedItemIndex);
     this.store.dispatch(
-      new ShoppingListActions.DeleteIngredients(this.editedItemIndex));
+      new ShoppingListActions.DeleteIngredients());
     this.onClear();
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+    this.store.dispatch(new ShoppingListActions.StopEdit());
   }
 
 }
--- course-project/src/app/shopping-list/store/shopping-list.reducer.ts
+++ course-project/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -32,23 +32,39 @@ export function ShoppingListReducer(state: State = initialState, action: Shoppin
         ...state,
         ingredients: [...state.ingredients, ...action.payload]
       };
-    case 'DELETE_INGREDIENT':
+    case ShoppingListActions.DELETE_INGREDIENT:
       return {
         ...state,
         ingredients: state.ingredients.filter((ig, igIndex) => {
-          return igIndex !== action.payload;
-        })
+          return igIndex !== state.editedIngredientIndex;
+        }),
+        editedIngredientIndex: -1,
+        editedIngredient: null
       };
-    case 'UPDATE_INGREDIENT':
-      const ingredient = state.ingredients[action.payload.index];
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[state.editedIngredientIndex];
       const updatedIngredient = {
-        ...ingredient, ...action.payload.ingredient
+        ...ingredient, ...action.payload
       };
       const updatedIngredients = [...state.ingredients];
-      updatedIngredients[action.payload.index] = updatedIngredient;
+      updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
       return {
         ...state,
-        ingredients: updatedIngredients
+        ingredients: updatedIngredients,
+        editedIngredientIndex: -1,
+        editedIngredient: null
+      };
+    case ShoppingListActions.START_EDIT:
+      return {
+        ...state,
+        editedIngredientIndex: action.payload,
+        editedIngredient: {...state.ingredients[action.payload]}
+      };
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredient: null,
+        editedIngredientIndex: -1
       };
     default:
       return state;
