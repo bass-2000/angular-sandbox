bc4c6f73d116a6c75b6dc9bec5728cb240983a1d
alexanderboyko <bass-2000@yandex.ru>
2020-08-10 19:23:55 +0300

    course-project Defining the First Effect

--- course-project/src/app/auth/store/auth.actions.ts
+++ course-project/src/app/auth/store/auth.actions.ts
@@ -1,7 +1,9 @@
 import {Action} from '@ngrx/store';
 
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
+export const LOGIN = '[Auth] Login';
+export const LOGOUT = '[Auth] Logout';
+export const LOGIN_START = '[Auth] Login Start';
+
 
 export class Login implements Action {
   readonly type = LOGIN;
