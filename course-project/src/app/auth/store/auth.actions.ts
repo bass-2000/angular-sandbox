4d5dc8cffa3456b2473face9043375990f1f7e04
alexanderboyko <bass-2000@yandex.ru>
2020-08-15 08:10:42 +0300

    course-project Adding Auto-Login with NgRx

--- course-project/src/app/auth/store/auth.actions.ts
+++ course-project/src/app/auth/store/auth.actions.ts
@@ -6,6 +6,7 @@ export const LOGOUT = '[Auth] Logout';
 export const LOGIN_START = '[Auth] AuthenticateSuccess Start';
 export const SIGNUP_START = '[Auth] Signup Start';
 export const CLEAR_ERROR = '[Auth] Clear Error';
+export const AUTO_LOGIN = '[Auth] Auto Login';
 
 
 export class AuthenticateSuccess implements Action {
@@ -45,10 +46,15 @@ export class ClearError implements Action {
   readonly type = CLEAR_ERROR;
 }
 
+export class AutoLogin implements Action {
+  readonly type = AUTO_LOGIN;
+}
+
 export type AuthActions =
   AuthenticateSuccess |
   Logout |
   LoginStart |
   AuthenticateFail |
   SignupStart |
-  ClearError;
+  ClearError |
+  AutoLogin;
