0b3749d41eef1a471081072ed422a3c00426d746
alexanderboyko <bass-2000@yandex.ru>
2020-08-17 18:58:57 +0300

    course-project cleanUp and adding StoreDevTools

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
@@ -12,6 +13,8 @@ import {StoreModule} from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import {EffectsModule} from '@ngrx/effects';
 import {AuthEffects} from './auth/store/auth.effects';
+import {environment} from '../environments/environment';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -25,6 +28,8 @@ import {AuthEffects} from './auth/store/auth.effects';
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
+    StoreRouterConnectingModule.forRoot(),
     SharedModule,
     CoreModule
   ],
--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -1,9 +1,7 @@
 import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {Router} from '@angular/router';
 import {Store} from '@ngrx/store';
 import {Subscription} from 'rxjs';
-import {AuthService} from './auth.service';
 import {AlertComponent} from '../shared/alert/alert.component';
 import {PlaceholderDirective} from '../shared/placeholder/placeholder.directive';
 import * as fromApp from '../store/app.reducer';
@@ -22,9 +20,7 @@ export class AuthComponent implements OnInit, OnDestroy {
   private closeSub: Subscription;
   private storeSub: Subscription;
 
-  constructor(private authService: AuthService,
-              private router: Router,
-              private componentFactoryResolver: ComponentFactoryResolver,
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,
               private store: Store<fromApp.AppState>) {
   }
 
@@ -39,34 +35,16 @@ export class AuthComponent implements OnInit, OnDestroy {
     const email = form.value.email;
     const password = form.value.password;
 
-    // let authObs: Observable<AuthResponseData>;
-
-    // this.isLoading = true;
     if (this.isLoginMode) {
-      // authObs = this.authService.login(email, password);
       this.store.dispatch(new AuthActions.LoginStart({email, password}));
     } else {
-      // authObs = this.authService.signup(email, password);
       this.store.dispatch(new AuthActions.SignupStart({email, password}));
     }
-
-
-    // authObs.subscribe(resData => {
-    //   console.log(resData);
-    //   this.isLoading = false;
-    //   this.router.navigate(['/recipes']);
-    // }, errorMessage => {
-    //   console.log(errorMessage);
-    //   this.error = errorMessage;
-    //   this.showErrorAlert(errorMessage);
-    //   this.isLoading = false;
-    // });
 
     form.reset();
   }
 
   onHandleError() {
-    // this.error = null;
     this.store.dispatch(new AuthActions.ClearError());
   }
 
