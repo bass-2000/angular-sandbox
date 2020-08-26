0448070c2ebb78e9c2e9dfb3602b114a8bae27d6
alexanderboyko <bass-2000@yandex.ru>
2020-08-26 20:00:38 +0300

    course-project Adding Angular Universal

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import * as fromApp from './store/app.reducer';
 import {LoggingService} from './logging.service';
 import {Store} from '@ngrx/store';
 import * as AuthActions from './auth/store/auth.actions';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,15 @@ import * as AuthActions from './auth/store/auth.actions';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store<fromApp.AppState>, private loggingService: LoggingService) {
+  constructor(private store: Store<fromApp.AppState>,
+              private loggingService: LoggingService,
+              @Inject(PLATFORM_ID) private platformId) {
   }
 
   ngOnInit(): void {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.loggingService.printLog('Hello from AppComponent ngOnInit');
   }
 
