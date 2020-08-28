52b8d86921af836c2193c68bfb179879aa2da4c1
alexanderboyko <bass-2000@yandex.ru>
2020-08-28 19:50:41 +0300

    angular-animations-guide: Animations triggers and state switching between states Transitions

--- angular-animations-guide/src/app/app.component.ts
+++ angular-animations-guide/src/app/app.component.ts
@@ -1,11 +1,27 @@
 import {Component} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800))
+    ])
+  ]
 })
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
 
   onAdd(item) {
     this.list.push(item);
@@ -14,5 +30,9 @@ export class AppComponent {
 
   onDelete(item) {
     this.list.splice(this.list.indexOf(item), 1);
+  }
+
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
   }
 }
